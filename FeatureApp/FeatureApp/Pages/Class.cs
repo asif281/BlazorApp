eb7e8e9858603899de208c4044cee7a5f9d49290
Asif Saeed <56203076+asif281@users.noreply.github.com>
2023-04-05 13:24:53 +0500

    Added Social Media LInks

---
+++ FeatureApp/FeatureApp/Pages/Class.cs
@@ -0,0 +1,49 @@
+﻿using AKSoftware.Localization.MultiLanguages;
+using AKSoftware.Localization.MultiLanguages.Blazor;
+using Microsoft.AspNetCore.Components;
+
+namespace FeatureApp.Pages
+{
+    public static class BlazorExtensions
+    {
+
+        /// <summary>
+        /// Track the state of the component, and it will be updated whenever the SetLanguage function has been called for the client 
+        /// </summary>
+        /// <param name="language">Langauge Container</param>
+        /// <param name="component">Component to be tracked</param>
+        public static void InitLocalizedComponent(this ILanguageContainerService language, ComponentBase component)
+        {
+            if (component == null)
+                throw new ArgumentNullException(nameof(component));
+
+            // Init the extension 
+            var extension = new ComponentExtension()
+            {
+                Component = component,
+            };
+
+            var action = new Action<object>(async e =>
+            {
+                // Retrieve the StateHasChanged method and the InvokeAsync of the dispatcher to run the code on the UI thread always
+                var type = typeof(ComponentBase);
+                var stateHasChangedMethod = type.GetMethod("StateHasChanged", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+                var dispatcherFunction = type.GetMethod("InvokeAsync",
+                                                        System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic,
+                                                        null,
+                                                        new Type[] { typeof(Action) },
+                                                        null);
+                // Run the state has changed in the InvokeAsync function
+                dispatcherFunction.Invoke(extension.Component, new[] { new Action(() =>
+                {
+                    stateHasChangedMethod.Invoke(extension.Component, null);
+                }) });
+
+            });
+
+            extension.Action = action;
+
+            language.AddExtension(extension);
+        }
+    }
+}
